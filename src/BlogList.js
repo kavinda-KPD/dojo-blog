60e843844f2d0a9731786ae6fb752f9ddb9f6ef6
kavinda-KPD <kavindadissanayake1999@gmail.com>
2023-07-09 23:54:45 +0530

    fetch data in a useEffect v-17

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,7 +2,7 @@
 //   const blogs = props.blogs;
 //   const title = props.title;
 
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -11,13 +11,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button
-            onClick={() => {
-              handleDelete(blog.id);
-            }}
-          >
-            Delete blog
-          </button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -2,34 +2,21 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  const [name, setName] = useState('mario');
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log('use effect is run');
-    console.log(name);
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('kavinda')}>Change the name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 };
