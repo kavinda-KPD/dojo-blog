32dc673a334b8a337fda72231f32f892a5a47483
kavinda-KPD <kavindadissanayake1999@gmail.com>
2023-07-10 07:13:34 +0530

    fetch data and catch error : Use effect ,upto v-19

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,41 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then((res) => {
+        if (!res.ok) {
+          throw Error('could not fetch the data for that resource');
+        }
+        console.log(res);
         return res.json();
       })
       .then((data) => {
+        setIsPending(false);
         setBlogs(data);
+        setError(null);
+      })
+      .catch((err) => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
       });
+
+    // setTimeout(() => {
+
+    // }, 1000);
   }, []);
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All blogs" />}
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 };
