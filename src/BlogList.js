544081ff817771bde7cf749792694d4022aba28f
kavinda-KPD <kavindadissanayake1999@gmail.com>
2023-07-10 11:11:50 +0530

    using of route parameters v-25

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,6 +2,8 @@
 //   const blogs = props.blogs;
 //   const title = props.title;
 
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
 const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
@@ -9,8 +11,10 @@ const BlogList = ({ blogs, title }) => {
 
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
