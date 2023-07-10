8a4c4792d46559db45b562049c482769eecf50f2
kavinda-KPD <kavindadissanayake1999@gmail.com>
2023-07-10 12:16:24 +0530

    get data to object from a form upto v-28

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,59 @@
+import { useState } from 'react';
+
 const Create = () => {
-  return <h1>Blog post create</h1>;
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>Blog title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+
+        <label>Blog Body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
+
+        <label>Blog Author</label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
+          <option value={'mario'}>mario</option>
+          <option value={'yoshi'}>yoshi</option>
+        </select>
+
+        <button>Add blog</button>
+
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
